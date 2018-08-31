9cf77f276d81e46d26d678903679bd31698ce05f
Josephine Nyoike <jnyoike@smith.edu>
2018-08-31 13:30:18 +0300

    Add files via upload

--- Actions.py
+++ Actions.py
@@ -7,10 +7,13 @@ class GetDigits(Action):
 		Action.__init__(self, tag= "GetDigits", children = children)
 
 class Play(Action):
-	def __init__(self):
-		Action.__init__(self, "Play")
+	def __init__(self, url):
+		attributes = {'url':url}
+		Action.__init__(self, "Play", attributes = attributes)
 
 class Say(Action):
 	def __init__(self, text, voice="woman", playBeep="false"):
+		voice = '"' + voice + '"'
+		playBeep = '"' + playBeep + '"'
 		attributes = {'voice': voice, 'playBeep':playBeep}
 		Action.__init__(self, tag = "Say", text = text, attributes = attributes)
